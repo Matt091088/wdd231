13856c785c436596c920e7e5a0a6fc7001de6c81
mac <macbookpro@MacBook-Pro-de-mac.local>
2025-09-15 20:17:08 -0300

    correcting some msitakes

--- scripts/navigation.js
+++ scripts/navigation.js
@@ -1,19 +1,7 @@
-// scripts/navigation.js
+// Menú hamburguesa responsive
+const menuBtn = document.getElementById('menu');
+const nav = document.querySelector('nav.navigation');
 
-// Seleccionamos el botón y la navegación
-const menuButton = document.querySelector("#menu");
-const navigation = document.querySelector(".navigation");
-
-// Evento para abrir/cerrar el menú en mobile
-menuButton.addEventListener("click", () => {
-  navigation.classList.toggle("show");
-  
-  // Cambiamos el ícono ☰ ↔ ✖
-  if (navigation.classList.contains("show")) {
-    menuButton.textContent = "✖";
-  } else {
-    menuButton.textContent = "☰";
-  }
+menuBtn.addEventListener('click', () => {
+  nav.classList.toggle('show');
 });
-
-
